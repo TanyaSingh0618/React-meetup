afe49f06ef6807fc4d5d6ec38b4390c32b7d6dda
Tanya Singh <63257659+TanyaSingh0618@users.noreply.github.com>
2023-06-22 17:02:09 +0530

    Add files via upload

---
+++ Favorites.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import FavContext from '../Store/context';
+import MeetupList from '../Components/Meetups/MeetupList';
+
+function Favorites() {
+    const FavCTX = useContext(FavContext);
+    let content;
+    if (FavCTX.totalFav === 0) {
+        content = <h1>EMPTY NO FAV HERE</h1>
+    } else {
+        content = <MeetupList meetups={FavCTX.favKey} />
+    }
+    return (
+        <section>
+            <h1>My Favorites</h1>
+            {content}
+        </section>
+    )
+}
+
+export default Favorites
